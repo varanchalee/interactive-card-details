76fd642d2cea67d9fc9eab00b85c07948aeb5d54
varanchaleekh <bollrin800@gmail.com>
2023-06-23 17:30:49 +0700

    add placeholder

--- index.js
+++ index.js
@@ -31,21 +31,57 @@ function addCardDetails() {
         <form>
             <div class="input-box">
                 <label for="input-holder-name">cardholder name</label>
-                <input type="text" id="input-holder-name" name="input-holder-name"/>
+                <input 
+                    type="text" 
+                    name="input-holder-name"
+                    id="input-holder-name"
+                    class="input-holder-name"
+                    placeholder="e.g. Jane Appleseed" 
+                    required
+                />
             </div>
             <div class="input-box">
                 <label for="input-card-number">card number</label>
-                <input type="text" id="input-card-number" name="input-card-number"/>
+                <input 
+                    type="text" 
+                    name="input-card-number"
+                    id="input-card-number" 
+                    class="input-card-number"
+                    placeholder="e.g. 1234 5678 9123 0000"
+                    required 
+                />
             </div>
             <div class="input-box">
                 <div class="form-date">
                     <label for="input-date">exp. date (MM/YY)</label>
-                    <input type="number" id="input-date" name="input-month"/>
-                    <input type="number" id="input-date" name="input-year"/>
+                    <input 
+                        type="number" 
+                        name="input-month"
+                        id="input-date" 
+                        class="input-date"
+                        placeholder="e.j. Jane Appleseed"
+                        required 
+                    />
+                    <input 
+                        type="number" 
+                        name="input-year"
+                        id="input-date" 
+                        class="input-date"
+                        placeholder="e.j. Jane Appleseed"
+                        required 
+
+                    />
                 </div>
                 <div class="form-cvc">
                     <label for="input-cvc">cvc</label>
-                    <input type="number" id="input-cvc" name="input-cvc"/>
+                    <input 
+                        type="number" 
+                        name="input-cvc"
+                        id="input-cvc" 
+                        class="input-cvc"
+                        placeholder="e.j. Jane Appleseed"
+                        required 
+                    />
                 </div>
             </div>    
         </form>
